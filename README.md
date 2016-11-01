fe85e7ff816413b51b11457c7ba1bad2b602d095
peter <jy02927731@yahoo.com.hk>
2016-11-01 23:43:28 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,102 @@
+# fb-bot
+
+SDK wrapper to verifyRequest, send message, recieve with Facebook Messenger Bots.
+
+[![NPM](https://nodei.co/npm/fb-bot.png?downloads=true&downloadRank=true)](https://www.npmjs.com/package/fb-bot)
+
+
+## Install
+
+```
+$ npm install --save fb-bot
+```
+
+# Initializing
+
+Initializing fb-bot 
+```js
+
+var express = require('express');
+var facebookModule = require('./index.js');
+var app = express();
+    
+facebookModule.init({
+  APP_SECRET: 'YOUR_APP_SECRET',
+  VALIDATION_TOKEN: 'YOUR_VALIDATION_TOKEN',
+  PAGE_ACCESS_TOKEN: 'YOUR_PAGE_ACCESS_TOKEN',
+  SERVER_URL: 'YOUR_SERVER_UR'
+});
+
+app.use(bodyParser.json({ verify: facebookModule.verifyRequestSignature }));
+```
+
+## Usage
+
+```js
+ ### 2 important funtions
+    facebookModule.sendTextMessage(_senderId, _message).then( ... )
+    facebookModule.messageListener(data, callback)
+
+```
+
+### Send an Echo message back to user
+```js
+//Facebook message 
+app.post('/webhook', facebookModule.parsePOST, function (req, res) {
+  var dataList = req.afterParse ;
+  dataList.forEach(function(data){
+    facebookModule.messageListener(data, function(){
+      facebookModule.sendTextMessage(data.senderId, data.message.text);
+    });
+  });
+  res.sendStatus(200);
+});
+```
+
+### Send url of attachments back to user
+```js
+//Facebook message 
+app.post('/webhook', facebookModule.parsePOST, function (req, res) {
+  var dataList = req.afterParse ;
+  dataList.forEach(function(data){
+    facebookModule.messageListener(data, function(){
+      facebookModule.sendTextMessage(data.senderId, data.message.attachments[0].payload.url);
+    });
+  });
+  res.sendStatus(200);
+});
+```
+
+
+## Two import routes
+#### Important
+ - GET     /webhook
+ - POST    /webhook
+ -  __**/webhook is default url routes where facebook send verification to**__
+
+```js
+##  Verification
+
+app.get('/webhook', facebookModule.authGET, function (req, res, next) {
+    res.status(200).send(req.query['hub.challenge']);
+});
+
+```
+
+```js
+## Recieve Message
+
+app.post('/webhook', facebookModule.parsePOST, function (req, res) {
+  var dataList = req.afterParse ;
+  dataList.forEach(function(data){
+      // ....
+  });
+  res.sendStatus(200);
+});
+```
+
+
+## License
+
+
+[MIT](http://vjpr.mit-license.org)
---
+++ app.js
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2016-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+/* jshint node: true, devel: true */
+'use strict';
+
+var bodyParser = require('body-parser'),
+    express = require('express'),
+    facebookModule = require('./index.js');
+    
+
+facebookModule.init({
+  APP_SECRET: 'YOUR_APP_SECRET',
+  VALIDATION_TOKEN: 'YOUR_VALIDATION_TOKEN',
+  PAGE_ACCESS_TOKEN: 'YOUR_PAGE_ACCESS_TOKEN',
+  SERVER_URL: 'YOUR_SERVER_UR'
+});
+
+var app = express();
+app.set('port', process.env.PORT || 5000);
+app.set('view engine', 'ejs');
+app.use(bodyParser.json({ verify: facebookModule.verifyRequestSignature }));
+app.use(express.static('public'));
+
+/*
+ * Use your own validation token. Check that the token used in the Webhook
+ * setup is the same token used here.
+ *
+ */
+app.get('/webhook', facebookModule.authGET, function (req, res, next) {
+    res.status(200).send(req.query['hub.challenge']);
+});
+
+/*
+ * All callbacks for Messenger are POST-ed. They will be sent to the same
+ * webhook. Be sure to subscribe your app to your page to receive callbacks
+ * for your page.
+ * https://developers.facebook.com/docs/messenger-platform/product-overview/setup#subscribe_app
+ *
+ */
+app.post('/webhook', facebookModule.parsePOST, function (req, res) {
+
+  var dataList = req.afterParse ;
+
+  dataList.forEach(function(data){
+    facebookModule.messageListener(data, function(){
+      facebookModule.sendTextMessage(data.senderId, data.message.text);
+    });
+  });
+
+  res.sendStatus(200);
+});
+
+/*
+ * This path is used for account linking. The account linking call-to-action
+ * (sendAccountLinking) is pointed to this URL.
+ *
+ */
+app.get('/authorize', function (req, res) {
+  var accountLinkingToken = req.query.account_linking_token;
+  var redirectURI = req.query.redirect_uri;
+
+  // Authorization Code should be generated per user by the developer. This will
+  // be passed to the Account Linking callback.
+  var authCode = "1234567890";
+
+  // Redirect users to this URI on successful login
+  var redirectURISuccess = redirectURI + "&authorization_code=" + authCode;
+
+  res.render('authorize', {
+    accountLinkingToken: accountLinkingToken,
+    redirectURI: redirectURI,
+    redirectURISuccess: redirectURISuccess
+  });
+});
+
+// Start server
+// Webhooks must be available via SSL with a certificate signed by a valid
+// certificate authority.
+app.listen(app.get('port'), function () {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+module.exports = app;
